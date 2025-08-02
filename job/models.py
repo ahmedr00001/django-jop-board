faebc07cface10d4af4edc7fe00280c86a961d67
Ahmed Hamdy <ahmedhandy06@gmail.com>
2025-08-02 18:14:29 +0300

    frontend pages and slug

---
+++ job/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.utils.text import slugify
+
+
+
+JOB_TYPE_CHOICES = (
+    ('ft', 'Full Time'),
+    ('pt', 'Part Time'),
+)
+
+
+class Category (models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+
+#function to customize upload folder
+def image_uploader(instance, imageName):
+    image_name, extension = imageName.rsplit(".", 1) 
+    return "job/%s.%s" % (instance.id if instance.id else "temp", extension)
+
+
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    jop_type= models.CharField(max_length=15 ,choices=JOB_TYPE_CHOICES)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateTimeField(auto_now= True)
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experience = models.IntegerField(default=1)
+    category = models.ForeignKey(Category , on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=image_uploader)
+    slug = models.SlugField(default="", null=True)
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+    
+    
+    def __str__(self):
+        return self.title
+
+
+
---
+++ job/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'job'
+
+urlpatterns = [
+    
+    path('', views.job_list, name='job_list'),
+    path('<str:slug>', views.job_detial, name='job_detial'),
+]
