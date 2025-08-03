7fd91ecabae33120267e268385c4099322364426
Ahmed Hamdy <ahmedhandy06@gmail.com>
2025-08-03 16:23:29 +0300

    Add New Jobs Page

--- job/urls.py
+++ job/urls.py
@@ -6,5 +6,6 @@ app_name = 'job'
 urlpatterns = [
     
     path('', views.job_list, name='job_list'),
+    path('add', views.add_job, name='add_job'),
     path('<str:slug>', views.job_detial, name='job_detial'),
 ]
--- job/views.py
+++ job/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from .models import Job , Apply
+from django.shortcuts import redirect, render
+from .models import Job , Apply , Category
 from django.core.paginator import Paginator
+from django.utils.text import slugify
+
 
 
 # Create your views here.
@@ -8,7 +10,7 @@ from django.core.paginator import Paginator
 def job_list(request):
     job_list = Job.objects.all()
 
-    paginator = Paginator(job_list, 1)
+    paginator = Paginator(job_list, 3)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
     context = {'jobs' : page_obj }
@@ -33,6 +35,38 @@ def job_detial(request , slug):
             cv=cv
         )
 
-
     context = {'job' : job}
     return render(request , 'job/job_detial.html' , context)
+
+def add_job (request):
+    job_type = Category.objects.all
+
+    if request.method =="POST":
+        title = request.POST.get('title')
+        jop_type = request.POST.get('jop_type')
+        category_name = request.POST.get('category')
+        category = Category.objects.get(name=category_name)
+        salary = request.POST.get('salary')
+        vacancy = request.POST.get('vacancy')
+        description = request.POST.get('description')
+        experience = request.POST.get('experience')
+        image = request.FILES.get('image')
+
+        Job.objects.create(
+            title = title,
+            jop_type= jop_type,
+            description = description,
+            vacancy = vacancy,
+            salary = salary,
+            experience = experience,
+            category = category,
+            image = image,
+            slug = slugify(title),
+        )
+
+        return redirect('jobs:job_list')
+
+
+
+    context = {'job_type' : job_type}
+    return render(request , 'jobs/add_Job.html' , context)
